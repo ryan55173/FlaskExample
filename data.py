88df389bccb71bb96a7865afe16da76eec5b2105
ryan55173 <rjd9183@gmail.com>
2020-06-29 12:36:53 -0400

    Adds extending templates

--- data.py
+++ data.py
@@ -1,6 +1,35 @@
+import sqlite3
+import random
+import logging
+import datetime
+import flask_wtf
+import wtforms
 
 
-class HeadData:
+# UserID is '8-digits' + '_' + '8-digits'
+# Token length is 128
+
+
+# Static
+alphanum_char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
+                 '8', '9', '0']
+special_char = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '`', '+', ',', '-', '.', '/', ':', ';', '<', '>',
+                '=', '?', '@', '[', ']', '\\', '^', '_', '~', '{', '}', '|', ]
+
+
+# Logging initialization
+logging.basicConfig(filename='logs\\site_log.log',
+                    format='%(asctime)s: %(message)s',
+                    datefmt='%y/%m/%d_%H:%M:%S',
+                    filemode='w+',
+                    level=logging.INFO)
+logging.info('Logging initialized')
+
+
+# Template and form stuff
+class BaseData:
     def __init__(self, **kwargs):
         self.title = kwargs.get('title', '')
         self.description = kwargs.get('description', '')
@@ -25,6 +54,120 @@ class Link:
         self.name = name
         self.url = url
 
+
+class LoginForm(flask_wtf.FlaskForm):
+    errors = []
+    username = wtforms.StringField('Username', validators=[wtforms.validators.DataRequired()])
+    password = wtforms.PasswordField('Password', validators=[wtforms.validators.DataRequired()])
+    submit_button = wtforms.SubmitField('Login')
+
+
+# Sql stuff
+def send_sql(sql_string):
+    conn = sqlite3.connect('account.db')
+    cursor = conn.cursor()
+    cursor.execute(sql_string)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def query_sql(sql_string):
+    conn = sqlite3.connect('account.db')
+    cursor = conn.cursor()
+    cursor.execute(sql_string)
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return results
+
+
+def write_new_user(user_id, username, password):
+    sql_string = 'INSERT INTO ACCOUNTS (USER_ID, USERNAME, PASSWORD)' \
+                 ' VALUES ("{}", "{}", "{}");'.format(user_id, username, password)
+    send_sql(sql_string)
+    log_string = 'Created new user: UserID="{}", Username="{}", Password="{}"'.format(user_id, username, password)
+    logging.info(log_string)
+
+
+def write_login(user_id):
+    # Get time
+    time = datetime.datetime.now()
+    time_now = time.strftime('%a_%H:%M:%S')
+    token = generate_token()
+    # Make token
+    sql_string = 'INSERT INTO LOGINS (USER_ID, TIME, TOKEN)' \
+                 ' VALUES ("{}", "{}", "{}");'.format(user_id, time_now, token)
+    send_sql(sql_string)
+    logging.info('Login from user_id: "{}"'.format(user_id))
+
+
+def token_exists(check_token):
+    results = query_sql('SELECT * FROM LOGINS;')
+    exists = False
+    for r in results:
+        if check_token == r[2]:
+            exists = True
+    return exists
+
+
+def generate_token():
+    token = rand_generate(128)
+    while token_exists(token):
+        token = rand_generate(128)
+    return token
+
+
+def username_to_user_id(username):
+    results = query_sql('SELECT * FROM ACCOUNTS;')
+    ret_user_id = None
+    for r in results:
+        name = r[1]
+        user_id = r[0]
+        if username == name:
+            ret_user_id = user_id
+            break
+    return ret_user_id
+
+
+def valid_login(username, password):
+    res = query_sql('SELECT * FROM ACCOUNTS WHERE USERNAME = "{}";'.format(username))
+    if len(res) != 1:
+        return False
+    result = res[0]
+    db_username = result[1]
+    db_password = result[2]
+    if (db_username == username) and (db_password == password):
+        return True
+    else:
+        return False
+
+
+def token_to_user_id(token):
+    res = query_sql('SELECT * FROM LOGINS WHERE TOKEN = "{}";'.format(token))
+    if len(res) < 1:
+        return None
+    result = res[0]
+    user_id = result[0]
+    return user_id
+
+
+# Other
+def rand_generate(num_digits, **kwargs):
+    allow_special = kwargs.get('allow_special', False)
+    allowed_list = []
+    allowed_list.extend(alphanum_char)
+    if allow_special:
+        allowed_list.extend(special_char)
+    last_index = len(allowed_list) - 1
+    digit_index = 0
+    rand_string = ''
+    while digit_index < num_digits:
+        pick_index = random.randint(0, last_index)
+        add_char = allowed_list[pick_index]
+        rand_string += add_char
+        digit_index += 1
+    return rand_string
 
 
 
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from data import *
 
 # Create the flask object
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'CZfad0nceTy19H7HoenUvQlNNCxRl4PmuJpkcRu3pb8rincKsMMovAcoPkmwPd2Df' \
+                           'inW2FPUX39mH8GrZ3U7o746zCtkxW6tcrdGoJ7enZJZjfdb9Mf30VfLE93wFQiU'
 nav_links = [
     Link('Home', '/index'),
     Link('About', '/about')
@@ -15,24 +17,49 @@ nav_links = [
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    index_data = HeadData(title='Home',
+    index_data = BaseData(title='Home',
                           description='Example flask website',
                           keywords=['Flask', 'Example', 'GitHub', 'Cat', 'Website'],
                           author='Badass Cat')
     index_data.make_keywords()
     return render_template('index.html',
                            data=index_data,
-                           nav_links=nav_links)
+                           nav_links=nav_links,
+                           user_tab=Link('Login', '/login'))  # TODO: Get if recent login
 
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
-    about_data = HeadData(title='About Author',
+    about_data = BaseData(title='About The Author',
                           description='Badass cat biography',
                           author='Badass Cat')
     return render_template('about.html',
                            data=about_data,
-                           nav_links=nav_links)
+                           nav_links=nav_links,
+                           user_tab=Link('Login', '/login'))  # TODO: Get if recent login
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_data = BaseData(title='Login',
+                          description='Login overlay',
+                          author='Badass Cat')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        password = login_form.password.data
+        # TODO: Write cookie and validate with db
+    return render_template('index.html',
+                           data=login_data,
+                           nav_links=nav_links,
+                           user_tab=Link('Login', '/login'),  # TODO: Get if recent login
+                           login=True,
+                           login_form=login_form)
+
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    pass
 
 
 # Flask entry point
